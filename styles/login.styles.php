097ae3000802d6dfdd5bf038b65aa14738b6f75b
AB <arpitbhat48@gmail.com>
2020-08-24 17:47:40 +0530

    added login page css

---
+++ styles/login.styles.php
@@ -0,0 +1,59 @@
+<style>
+	label {
+		color: #EEEEEE;
+		font-weight: 500;
+		font-style: italic;
+	}
+
+	h1 {
+		margin: auto;
+		color: #4F8A8B;
+		width: 70%;
+		font-size: 50px;
+		margin-top: 50px;
+	}
+
+	.centre {
+		margin: auto;
+		text-align: centre;
+		width: 40%;
+	}
+
+	.inputs {
+		width: 100%;
+		padding: 16px 20px;
+		margin: 8px 0;
+		display: inline-block;
+		box-sizing: border-box;
+	}
+
+	form {	
+		background-color: #222831;
+		padding: 65px 50px 50px 50px;
+		font-size: 1.4em;
+		margin: 50px;
+		border-radius: 20px;		
+	}
+
+	form a {
+		color: #4F8A8B;
+		font-size: 0.70em;
+		float: right;
+		margin-top: 5px;
+	}
+
+	.btn {
+		margin: 10px 0;
+		background-color: #222831;
+		color: #FBD46D;
+		font-weight: 500;
+		border: 5px solid #FBD46D;
+		border-radius: 30px;
+		font-size: 1.1em;
+		padding: 7px 40px;
+		cursor: pointer;
+	}
+	.login {
+		float:right;
+	}
+</style>	
