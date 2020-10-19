f7a6978e9d3e03424dd153c036eca6968dd637aa
AB <arpitbhat48@gmail.com>
2020-10-19 11:03:30 +0530

    cart checkout php working

--- cart-page.php
+++ cart-page.php
@@ -8,7 +8,7 @@ require('./components/page-title/page-title.php');
 require("db/db.php");
 ?>
  
- <?php
+<?php
 
 if(isset($_SESSION['rollno'])){
     $rno =$_SESSION['rollno'];
@@ -22,26 +22,25 @@ if(isset($_SESSION['rollno'])){
     
     page_title("Cart");
     require('./components/cart-page-card/cart-page-card.php');
-    ?>
+?>
 </div>
 <div class="container container-grid">
     
-    
-    
-    <div class="cart-item grid-item">
+    <div class="cart-item">
         <?php
         $qry = "SELECT i.item_name, i.cost, c.id 
                 FROM cart c, inventory i 
                 WHERE c.item_id = i.item_id
                 AND c.user_rno = '$rno'";
         $result = mysqli_query($con, $qry);
-        
+
         while ($row = mysqli_fetch_assoc($result)) {
             cart_page_card (
                 $row["item_name"],
                 $row["cost"],
                 $row["id"]
             );
+            
         }
         ?>
     </div>
@@ -49,31 +48,33 @@ if(isset($_SESSION['rollno'])){
         <div class=" text-center">
             <h2 class="checkout-heading">Checkout <h2>
         </div>
-
-        <div class="price">₹501</div>
-        <div class="price">+ ₹220</div>
-        <div class="price">+ ₹100</div>
-        <div class="price">+ ₹40</div>
-        <div class="price">+ ₹40</div>
-
-        <div class="price">+ ₹40</div>
-
-        <div class="price">+ ₹40</div>
-
-        <div class="price">+ ₹40</div>
-
-        <div class="price">+ ₹40</div>
+        <?php 
+        $result = mysqli_query($con, $qry);
+        $total = 0;
+        while ($row = mysqli_fetch_assoc($result)) {
+            $name = $row['item_name'];
+            $price = $row['cost'];
+            $total += $price;
+            echo "
+            <div>
+                <span class=\"checkout-item\"> $name </span>
+                <span class=\"price\"> $price </span>
+            </div>
+            ";
+            // echo $total;
+        }
+        ?>
         
         <div>
             <hr>
-            <h2 class="">Total : ₹ <?php $total ?></h2>
+            <h2 class="">Total : ₹ <?php echo $total ?></h2>
             <div class="">
                 <button class="checkout-btn">PROCEED TO PAY</button>
             </div>
         </div>
     </div>
-       
 </div>
+
 <?php
 require("./components/footer/footer.php");
 ?>
