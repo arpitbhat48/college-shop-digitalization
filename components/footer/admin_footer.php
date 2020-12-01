9513e9007eadebaab19d8247b585d42bcf91511f
Archit Bhonsle <abhonsle2000@gmail.com>
2020-12-01 12:51:24 +0530

    insert page styling

---
+++ components/footer/admin_footer.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../components/footer/footer.css">
+
+<div class="push"></div>
+</div>
+<footer>
+    Made with 😡 by Archit, Arpit and Omkar
+</footer>
+
+</body>
+
+</html>
